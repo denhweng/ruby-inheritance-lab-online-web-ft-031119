e3f143a80dfd0e76567019b9e23042d8dfc705b0
Dan Hwang <danhwangfkpc@gmail.com>
2019-04-17 01:53:25 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,9 @@
 class User 
   attr_accessor :first_name, :last_name 
   
-  @knowledge = []
-  def initialize(first_name, last_name)
-    @first_name = first_name 
-    @last_name = last_name 
+ 
+  def initialize
+     @knowledge = []
   end 
   
   def learn 
